da0bc309aa61311cf9aa16785774813419df5eec
Hari Gurusubramanian <guru0026@algonquinlive.com>
2023-04-15 21:03:11 -0400

    adds script for accessing api

---
+++ script.js
@@ -0,0 +1,26 @@
+const APIKey = 'rcJGnaiUc6VrYDVi4aoyHLEoHPcv5Gp3dlZ1jHsP';
+const form = document.querySelector('form');
+const imageContainer = document.querySelector('#image-container');
+const saveButton = document.querySelector('#save');
+const favoritesContainer = document.querySelector('#favorites-container');
+const favorites = [];
+
+function fetchImageData(event) {
+    event.preventDefault();
+    const date = document.querySelector('#date').value;
+    const url = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            imageContainer.innerHTML = `
+                <img src="${data.url}" alt="${data.title}">
+                <h2>${data.title}</h2>
+                <p>${data.explanation}</p>
+            `;
+            saveButton.style.display = 'block';
+        })
+        .catch(error => console.error(error));
+}
+
+form.addEventListener('submit', fetchImageData);
+saveButton.addEventListener('click', saveImage);
